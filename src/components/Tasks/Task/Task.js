0b39415783b925c67c1d302fe5aae2f5dbf9a795
PoizonedCookie <128691950+PoizonedCookie@users.noreply.github.com>
2023-10-08 11:00:54 -0400

    add single Task component and its styles

---
+++ src/components/Tasks/Task/Task.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./styles";
+import { View, Text } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+
+export default Task = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Todo App</Text>
+            <Text>by John Doe</Text>
+        </View>
+    )
+}
