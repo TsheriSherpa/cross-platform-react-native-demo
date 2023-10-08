555e38e85a8824bea20ed3fa18d3fdb92e04362c
PoizonedCookie <128691950+PoizonedCookie@users.noreply.github.com>
2023-10-08 13:05:14 -0400

    changed style for list view of task

--- src/components/Tasks/Task/styles.js
+++ src/components/Tasks/Task/styles.js
@@ -2,11 +2,27 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+        padding: 10,
+        backgroundColor: "dimgrey",
+        marginBottom: 10,
     },
+    header: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderBottomWidth: 2,
+    },
+    headerIconView: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    title: {
+        fontWeight: 'bold',
+    },
+    editIcon: {
+        marginRight: 20
+    }
 });
 
 export default styles;
--- src/styles/main.js
+++ src/styles/main.js
@@ -2,10 +2,10 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+        // alignItems: 'center',
+        // justifyContent: 'center',
     },
 });
 
