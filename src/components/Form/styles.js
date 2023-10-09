bac370eda9e485f98f2c76a75cd1e433dbc92cd6
PoizonedCookie <128691950+PoizonedCookie@users.noreply.github.com>
2023-10-09 15:54:09 -0400

    error message css optimized

--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -2,7 +2,11 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     modalContainer: {
-        padding: 20
+        padding: 20,
+        borderWidth: 2,
+        margin: 20,
+        borderRadius: 10,
+        backgroundColor: 'lightgrey'
     },  
     header: {
         height: 60,
@@ -41,6 +45,21 @@ const styles = StyleSheet.create({
     addButton: {
         borderRadius: 10,
         width: 10
+    },
+    errorMessageView: {
+        borderWidth: 1,
+        borderColor: 'red',
+        padding: 10,
+        shadowColor: 'rgba(0, 0, 0, 0.75)',
+        shadowOffset: { width: 2, height: 2 },
+        shadowOpacity: 0.5,
+        elevation: 5,
+        marginTop: 20,
+    },
+
+    errorMessage: {
+        color: 'red',
+        fontWeight: 'bold'
     }
 });
 
